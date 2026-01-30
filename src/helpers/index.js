9fac0ab7fa9dc50e148b21705fad316b0ea0dc2c
Giselle McNeill <giselle.mcneill77@gmail.com>
2026-01-30 00:12:30 +0000

    Add helper to evaluate quiz answers and return a message

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,3 +3,4 @@ import 'dotenv/config'; // load .env automatically
 export { loadEvents } from "./loadEvents.js";
 export { loadCommands } from "./loadCommands.js";
 export { default as loadFiles } from "./loadFiles.js";
+export { evaluateAnswer } from "./evaluateAnswer.js";
